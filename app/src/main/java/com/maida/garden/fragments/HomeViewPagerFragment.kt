f169a009fe33bcb7994889795a4532b0187bcdc8
Suresh Maidaragi <maidaragi1919@gmail.com>
2020-06-11 18:09:46 +0530

    viewpager

---
+++ app/src/main/java/com/maida/garden/fragments/HomeViewPagerFragment.kt
@@ -0,0 +1,18 @@
+package com.maida.garden.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
